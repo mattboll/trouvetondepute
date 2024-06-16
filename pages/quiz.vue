f7239d07ec473b87236b0ff8b0587280852fa4ed
Matthieu Bollot <m.bollot@code-troopers.com>
2024-06-17 00:44:06 +0200

    feat: init tailwind

--- pages/quiz.vue
+++ pages/quiz.vue
@@ -1,11 +1,11 @@
 <template>
-    <div>
-        <h1>Quiz</h1>
-        <div v-for="question in questions" :key="question.id">
-            <h2>{{ question.text }}</h2>
-            {{  question }}
-        </div>
+  <div>
+    <h1 class="text-3xl font-bold underline">Quiz</h1>
+    <div v-for="question in questions" :key="question.id">
+      <h2>{{ question.text }}</h2>
+      {{ question }}
     </div>
+  </div>
 </template>
 
 <script setup>
@@ -13,5 +13,4 @@ import { questions } from "../data/questions.js";
 import { useQuizStore } from "../stores/quiz";
 
 const quizStore = useQuizStore();
-
 </script>
